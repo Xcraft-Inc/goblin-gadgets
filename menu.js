ddbc8d8ee68aee4ac84f3f83a2fb4adb80727743
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-06-13 11:59:11 +0200

    Use the auto generated RC output
    Note that it's limited by shellcraft, for example, the cases
    where only an optional argument with or without array is needed
    is not supported.

--- menu.js
+++ menu.js
@@ -5,49 +5,6 @@
  *
  * @returns {Object} The list and definitions of commands.
  */
-const servicePath = './widgets/menu/service.js';
 exports.xcraftCommands = function () {
-  return {
-    handlers: require (servicePath).handlers,
-    context: require (servicePath).context,
-    rc: {
-      create: {
-        parallel: true,
-        desc: 'xxx',
-        options: {
-          params: {
-            required: 'id',
-            optional: 'params',
-          },
-        },
-      },
-      text: {
-        parallel: true,
-        desc: 'xxx',
-        options: {
-          params: {
-            required: 'id',
-          },
-        },
-      },
-      kind: {
-        parallel: true,
-        desc: 'xxx',
-        options: {
-          params: {
-            required: 'id',
-          },
-        },
-      },
-      delete: {
-        parallel: true,
-        desc: 'xxx',
-        options: {
-          params: {
-            required: 'id',
-          },
-        },
-      },
-    },
-  };
+  return require (`./widgets/${require ('path').basename (__filename, '.js')}/service.js`);
 };
