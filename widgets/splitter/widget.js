d606256cbd80f00c2a6ffa0684d051fe34e19a39
Daniel Roux <roux@epsitec.ch>
2017-06-16 17:16:11 +0200

    New Splitter is functional.

--- widgets/splitter/widget.js
+++ widgets/splitter/widget.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Widget from 'laboratory/widget';
 
 /******************************************************************************/
@@ -6,6 +7,63 @@ import Widget from 'laboratory/widget';
 class Splitter extends Widget {
   constructor (props) {
     super (props);
+    this.state = {
+      splitterValue: '200px',
+    };
+  }
+
+  get splitterValue () {
+    return this.state.splitterValue;
+  }
+
+  set splitterValue (value) {
+    this.setState ({
+      splitterValue: value,
+    });
+  }
+
+  getOffset (x, y) {
+    const node = ReactDOM.findDOMNode (this.resizer);
+    const rect = node.getBoundingClientRect ();
+    const kind = this.read ('kind');
+    if (kind === 'vertical') {
+      if (x >= rect.left && x <= rect.right) {
+        return x - rect.left;
+      }
+    } else {
+      if (y >= rect.top && y <= rect.bottom) {
+        return y - rect.top;
+      }
+    }
+    return -1;
+  }
+
+  onMouseMove (e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    if (e.buttons === 1) {
+      // Mouse left button pressed ?
+      if (!this.isDragging) {
+        const offset = this.getOffset (x, y);
+        if (offset !== -1) {
+          this.offset = offset;
+          this.isDragging = true;
+        }
+      }
+      if (this.isDragging) {
+        const node = ReactDOM.findDOMNode (this.leftPane);
+        const rect = node.getBoundingClientRect ();
+        const kind = this.read ('kind');
+        if (kind === 'vertical') {
+          this.splitterValue = x - this.offset - rect.left + 'px';
+        } else {
+          this.splitterValue = y - this.offset - rect.top + 'px';
+        }
+      }
+    } else {
+      // Mouse left button released ?
+      this.isDragging = false;
+    }
   }
 
   widget () {
@@ -26,13 +84,19 @@ class Splitter extends Widget {
       const resizerStyle = this.styles.resizer;
       const rightPaneStyle = this.styles.rightPane;
 
+      if (kind === 'vertical') {
+        leftPaneStyle.width = this.splitterValue;
+      } else {
+        leftPaneStyle.height = this.splitterValue;
+      }
+
       return (
-        <div style={containerStyle}>
-          <div style={leftPaneStyle}>
+        <div style={containerStyle} onMouseMove={::this.onMouseMove}>
+          <div style={leftPaneStyle} ref={node => (this.leftPane = node)}>
             {children[0]}
           </div>
-          <div style={resizerStyle} />
-          <div style={rightPaneStyle}>
+          <div style={resizerStyle} ref={node => (this.resizer = node)} />
+          <div style={rightPaneStyle} ref={node => (this.rightPane = node)}>
             {children[1]}
           </div>
         </div>
