f91c6def0ef954b8617b4402803eddee1ea3d428
Daniel Roux <roux@epsitec.ch>
2017-07-18 12:07:43 +0200

    Change data format and use convertToMapWithIds.

--- widgets/wizard-button/service.js
+++ widgets/wizard-button/service.js
@@ -1,18 +1,15 @@
 'use strict';
 
 const path = require ('path');
-
 const goblinName = path.basename (module.parent.filename, '.js');
-
 const Goblin = require ('xcraft-core-goblin');
+const uuidV4 = require ('uuid/v4');
 
 // Define initial logic values
 const logicState = {};
 
-const params = {
-  kind: {
-    id: 'kind',
-    order: 0,
+const data = [
+  {
     type: 'combo',
     field: 'kind',
     list: [
@@ -54,146 +51,124 @@ const params = {
     ],
     value: '',
   },
-  text: {
-    id: 'text',
-    order: 1,
+  {
     type: 'text',
     field: 'text',
     value: 'Abc',
   },
-  glyph: {
-    id: 'glyph',
-    order: 2,
+  {
     type: 'text',
     field: 'glyph',
     list: ['', 'check', 'bicycle', 'car', 'rocket', 'calendar'],
     value: 'check',
   },
-  tooltip: {
-    id: 'tooltip',
-    order: 3,
+  {
     type: 'text',
     field: 'tooltip',
     value: '',
   },
-  shortcut: {
-    id: 'shortcut',
-    order: 4,
+  {
     type: 'text',
     field: 'shortcut',
     list: ['', '_ctrl_+A', '_shift_+A', '_alt_+A'],
     value: '',
   },
-  justify: {
-    id: 'justify',
-    order: 5,
+  {
     type: 'combo',
     field: 'justify',
     list: ['', 'flex-start', 'center', 'flex-end'],
     value: 'center',
   },
-  width: {
-    id: 'width',
-    order: 6,
+  {
     type: 'text',
     field: 'width',
     list: ['', '32px', '50px', '100px', '200px', '300px', '500px'],
     value: '',
   },
-  height: {
-    id: 'height',
-    order: 7,
+  {
     type: 'text',
     field: 'height',
     list: ['', '32px', '50px', '100px', '200px'],
     value: '',
   },
-  grow: {
-    id: 'grow',
-    order: 8,
+  {
     type: 'text',
     field: 'grow',
     list: ['', '0.5', '1'],
     value: '',
   },
-  spacing: {
-    id: 'spacing',
-    order: 9,
+  {
     type: 'combo',
     field: 'spacing',
     list: ['', 'overlap', 'tiny', 'large'],
     value: '',
   },
-  glyphColor: {
-    id: 'glyphColor',
-    order: 10,
+  {
     type: 'text',
     field: 'glyphColor',
     list: ['', 'primary', 'secondary', 'base', '#fff', '#888', '#000', '#f00'],
     value: '',
   },
-  textColor: {
-    id: 'textColor',
-    order: 11,
+  {
     type: 'text',
     field: 'textColor',
     list: ['', 'primary', 'secondary', 'base', '#fff', '#888', '#000', '#f00'],
     value: '',
   },
-  textTransform: {
-    id: 'textTransform',
-    order: 12,
+  {
     type: 'combo',
     field: 'textTransform',
     list: ['', 'uppercase'],
     value: '',
   },
-  badgeValue: {
-    id: 'badgeValue',
-    order: 13,
+  {
     type: 'text',
     field: 'badgeValue',
     list: ['', '1', '2', '99'],
     value: '',
   },
-  glyphPosition: {
-    id: 'glyphPosition',
-    order: 14,
+  {
     type: 'combo',
     field: 'glyphPosition',
     list: ['', 'left', 'right'],
     value: '',
   },
-  place: {
-    id: 'place',
-    order: 15,
+  {
     type: 'combo',
     field: 'place',
     list: ['', '1/1', '1/3', '2/3', '3/3'],
     value: '',
   },
-  active: {
-    id: 'active',
-    order: 16,
+  {
     type: 'bool',
     field: 'active',
     value: 'false',
   },
-  show: {
-    id: 'show',
-    order: 17,
+  {
     type: 'bool',
     field: 'show',
     value: 'true',
   },
-  visibility: {
-    id: 'visibility',
-    order: 18,
+  {
     type: 'bool',
     field: 'visibility',
     value: 'true',
   },
-};
+];
+
+function convertToMapWithIds (data) {
+  const result = {};
+  let order = 0;
+  for (const item of data) {
+    const id = uuidV4 ();
+    item.id = id;
+    item.order = order++;
+    result[id] = item;
+  }
+  return result;
+}
+
+const params = convertToMapWithIds (data);
 
 // Define logic handlers according rc.json
 const logicHandlers = {
