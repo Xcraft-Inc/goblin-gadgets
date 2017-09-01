fd9ec180a73cf3b38fc9717e87d7769463f07889
Daniel Roux <roux@epsitec.ch>
2017-09-01 14:47:22 +0200

    Add component task-menu.

---
+++ widgets/task-menu/styles.js
@@ -0,0 +1,30 @@
+/******************************************************************************/
+
+export default function styles (_theme, _props) {
+  const mainStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+  };
+
+  const tableStyle = {
+    marginTop: '20px',
+  };
+
+  const footerStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    height: '80px',
+    overflowX: 'hidden',
+    overflowY: 'hidden',
+    marginTop: '10px',
+  };
+
+  return {
+    main: mainStyle,
+    table: tableStyle,
+    footer: footerStyle,
+  };
+}
+
+/******************************************************************************/
---
+++ widgets/task-menu/widget.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import Widget from 'laboratory/widget';
+import * as GlyphHelpers from '../helpers/glyph-helpers.js';
+import * as ComboHelpers from '../helpers/combo-helpers.js';
+import * as Bool from '../helpers/boolean-helpers.js';
+import {ColorHelpers} from 'electrum-theme';
+import {Unit} from 'electrum-theme';
+
+import DialogModal from 'gadgets/dialog-modal/widget';
+import Container from 'gadgets/container/widget';
+import Button from 'gadgets/button/widget';
+import Label from 'gadgets/label/widget';
+import Table from 'gadgets/table/widget';
+import LabelTextField from 'gadgets/label-text-field/widget';
+
+/******************************************************************************/
+
+class TaskMenu extends Widget {
+  constructor () {
+    super (...arguments);
+  }
+
+  onClose () {
+    const x = this.props.closeDialog;
+    if (x) {
+      x ();
+    }
+  }
+
+  onSelectAll () {}
+
+  renderMain () {
+    const mainClass = this.styles.classNames.main;
+    const tableClass = this.styles.classNames.table;
+    return (
+      <div className={mainClass}>
+        <Container kind="row">
+          <Label text="Activités" grow="1" kind="title" />
+          <Button
+            glyph="check"
+            tooltip="Sélectionne tout"
+            onClick={::this.onSelectAll}
+          />
+        </Container>
+        <div className={tableClass}>
+          <Table height="200px" data={this.props.data} enableSelection="true" />
+        </div>
+      </div>
+    );
+  }
+
+  render () {
+    const footerClass = this.styles.classNames.footer;
+
+    return (
+      <DialogModal
+        width={this.props.width}
+        center={this.props.center}
+        left={this.props.left}
+        close={::this.onClose}
+      >
+        {this.renderMain ()}
+        <div className={footerClass}>
+          <Label grow="1" />
+          <Button
+            glyph="check"
+            text="Ouvrir"
+            kind="action"
+            width="150px"
+            place="1/1"
+            onClick={::this.onClose}
+          />
+        </div>
+      </DialogModal>
+    );
+  }
+}
+
+/******************************************************************************/
+export default TaskMenu;
