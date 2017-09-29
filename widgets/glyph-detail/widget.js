41f74b5cca2555972efa2a545b0b8e84a417c241
Daniel Roux <roux@epsitec.ch>
2017-09-29 08:12:47 +0200

    Implement panel glyph-detail.

--- widgets/glyph-detail/widget.js
+++ widgets/glyph-detail/widget.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Widget from 'laboratory/widget';
+
 import Container from 'gadgets/container/widget';
 import Label from 'gadgets/label/widget';
+import Button from 'gadgets/button/widget';
 
 class GlyphDetail extends Widget {
   constructor () {
     super (...arguments);
+    this.onEdit = this.onEdit.bind (this);
   }
 
   static get wiring () {
@@ -15,13 +18,49 @@ class GlyphDetail extends Widget {
     };
   }
 
+  onEdit () {
+    const contact = this.getEntityById (this.props.entityId).toJS ();
+    this.do ('edit', {contact});
+  }
+
   render () {
     const labelOf = this.getWidgetToEntityMapper (Label, 'text');
     const Title = labelOf ('.name');
+    const Name = labelOf ('.name');
+    const Glyph = this.getWidgetToEntityMapper (Label, 'glyph') ('.glyph');
+
     return (
       <Container kind="column-full">
         <Container kind="pane-header">
           <Title kind="pane-header" />
+        </Container>
+        <Container kind="panes">
+          <Container kind="pane">
+            <Container kind="row-pane">
+              <Label text="Pictogramme" kind="title" />
+            </Container>
+            <Container kind="row-pane">
+              <Name />
+            </Container>
+            <Container kind="row-pane">
+              <Glyph
+                glyphSize="500%"
+                height="150px"
+                grow="1"
+                justify="center"
+                insideButton="true"
+              />
+            </Container>
+            <Container kind="row-pane" subkind="footer">
+              <Button
+                kind="subaction"
+                text="Editer"
+                width="0px"
+                grow="1"
+                onClick={this.onEdit}
+              />
+            </Container>
+          </Container>
         </Container>
       </Container>
     );
