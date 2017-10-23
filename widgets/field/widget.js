c155789a62a0dd1644fd27f4f7418b8e2bf20798
Daniel Roux <roux@epsitec.ch>
2017-10-23 16:17:02 +0200

    Add property readonly to Field.

--- widgets/field/widget.js
+++ widgets/field/widget.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from 'laboratory/form';
 import {Control} from 'react-redux-form/immutable';
+import * as Bool from '../helpers/boolean-helpers.js';
 
 import Container from 'gadgets/container/widget';
 import Button from 'gadgets/button/widget';
@@ -45,35 +46,42 @@ class Field extends Form {
     }
   }
 
-  renderField () {
+  //#region Readonly
+  renderReadonlyField () {
+    const Value = this.mapWidget (
+      Label,
+      value => {
+        return {text: value};
+      },
+      this.fullPath
+    );
+
     return (
       <Container
         kind="row-pane"
         width={this.props.width}
         height={this.props.height}
       >
-
-        <LabelTextField
-          labelText={this.props.labelText}
-          labelGlyph={this.props.labelGlyph}
-          labelWidth={this.props.labelWidth || defaultLabelWidth}
-          hintText={this.props.hintText}
-          rows={this.props.rows}
-          model={this.props.model}
-          grow="1"
+        <Label
+          text={this.props.labelText}
+          glyph={this.props.labelGlyph}
+          width={this.props.labelWidth || defaultLabelWidth}
+          kind="label-text-field"
+          justify="left"
+          spacing="overlap"
         />
+        <Value grow="1" />
       </Container>
     );
   }
 
-  renderDoubleField () {
+  renderReadonlyDoubleField () {
     return (
       <Container
         kind="row-pane"
         width={this.props.width}
         height={this.props.height}
       >
-
         <Label
           text={this.props.labelText}
           glyph={this.props.labelGlyph}
@@ -97,7 +105,7 @@ class Field extends Form {
     );
   }
 
-  renderCombo () {
+  renderReadonlyCombo () {
     return (
       <Container
         kind="row-pane"
@@ -129,7 +137,7 @@ class Field extends Form {
     );
   }
 
-  renderRadio () {
+  renderReadonlyRadio () {
     const Radio = this.mapWidget (
       RadioList,
       value => {
@@ -172,7 +180,7 @@ class Field extends Form {
     );
   }
 
-  renderBool () {
+  renderReadonlyBool () {
     const Check = this.mapWidget (
       CheckButton,
       value => {
@@ -206,7 +214,7 @@ class Field extends Form {
     );
   }
 
-  renderHinter () {
+  renderReadonlyHinter () {
     const Hinter = this.mapWidget (
       LabelTextField,
       value => {
@@ -244,7 +252,7 @@ class Field extends Form {
     );
   }
 
-  renderFileInput () {
+  renderReadonlyFileInput () {
     return (
       <Container kind="row-pane">
         <input
@@ -255,25 +263,266 @@ class Field extends Form {
       </Container>
     );
   }
+  //#endregion
 
-  render () {
+  //#region Edit
+  renderEditField () {
+    return (
+      <Container
+        kind="row-pane"
+        width={this.props.width}
+        height={this.props.height}
+      >
+        <LabelTextField
+          labelText={this.props.labelText}
+          labelGlyph={this.props.labelGlyph}
+          labelWidth={this.props.labelWidth || defaultLabelWidth}
+          hintText={this.props.hintText}
+          rows={this.props.rows}
+          model={this.props.model}
+          grow="1"
+        />
+      </Container>
+    );
+  }
+
+  renderEditDoubleField () {
+    return (
+      <Container
+        kind="row-pane"
+        width={this.props.width}
+        height={this.props.height}
+      >
+        <Label
+          text={this.props.labelText}
+          glyph={this.props.labelGlyph}
+          width={this.props.labelWidth || defaultLabelWidth}
+          kind="label-text-field"
+          justify="left"
+          spacing="overlap"
+        />
+        <TextField
+          hintText={this.props.hintText1}
+          model={this.props.model1}
+          grow={this.props.growField1}
+          spacing="large"
+        />
+        <TextField
+          hintText={this.props.hintText2}
+          model={this.props.model2}
+          grow={this.props.growField2}
+        />
+      </Container>
+    );
+  }
+
+  renderEditCombo () {
+    return (
+      <Container
+        kind="row-pane"
+        width={this.props.width}
+        height={this.props.height}
+      >
+        <Label
+          text={this.props.labelText}
+          glyph={this.props.labelGlyph}
+          width={this.props.labelWidth || defaultLabelWidth}
+          kind="label-text-field"
+          justify="left"
+          spacing="overlap"
+        />
+        <TextFieldCombo
+          hintText={this.props.hintText}
+          model={this.props.model}
+          readonly="false"
+          list={this.props.list}
+          menuType="wrap"
+          menuItemWidth={this.props.menuItemWidth}
+          comboTextTransform="none"
+          onSetText={text => {
+            this.setBackendValue (this.fullPath, text);
+          }}
+          grow="1"
+        />
+      </Container>
+    );
+  }
+
+  renderEditRadio () {
+    const Radio = this.mapWidget (
+      RadioList,
+      value => {
+        if (value && value !== '') {
+          return {selectedIndex: this.props.list.indexOf (value)};
+        } else {
+          return {};
+        }
+      },
+      this.fullPath
+    );
+
+    return (
+      <Container
+        kind="row-pane"
+        subkind="left"
+        width={this.props.width}
+        height={this.props.height}
+      >
+        <Label
+          text={this.props.labelText}
+          glyph={this.props.labelGlyph}
+          width={this.props.labelWidth || defaultLabelWidth}
+          kind="label-text-field"
+          justify="left"
+          spacing="overlap"
+        />
+        <Radio
+          height={this.props.height}
+          direction={this.props.direction || 'row'}
+          list={this.props.list}
+          selectionChanged={index => {
+            this.setBackendValue (this.fullPath, this.props.list[index]);
+            if (this.props.onChange) {
+              this.props.onChange (this.props.list[index], index);
+            }
+          }}
+        />
+      </Container>
+    );
+  }
+
+  renderEditBool () {
+    const Check = this.mapWidget (
+      CheckButton,
+      value => {
+        return {checked: value};
+      },
+      this.fullPath
+    );
+
+    return (
+      <Container
+        kind="row-pane"
+        subkind="left"
+        width={this.props.width}
+        height={this.props.height}
+      >
+        <Label
+          glyph={this.props.labelGlyph}
+          width={this.props.labelWidth || defaultLabelWidth}
+          kind="label-text-field"
+          justify="left"
+          spacing="overlap"
+        />
+        <Check
+          text={this.props.labelText}
+          onClick={index => {
+            const checked = this.getBackendValue (this.fullPath);
+            this.setBackendValue (this.fullPath, !checked);
+          }}
+        />
+      </Container>
+    );
+  }
+
+  renderEditHinter () {
+    const Hinter = this.mapWidget (
+      LabelTextField,
+      value => {
+        if (value && value !== '') {
+          return {selectedValue: value};
+        } else {
+          return {};
+        }
+      },
+      this.fullPath
+    );
+
+    const Form = this.Form;
+
+    return (
+      <Container
+        kind="row-pane"
+        width={this.props.width}
+        height={this.props.height}
+      >
+        <Form
+          {...this.formConfigWithComponent (() => (
+            <Hinter
+              labelText={this.props.labelText}
+              labelGlyph={this.props.labelGlyph}
+              labelWidth={this.props.labelWidth || defaultLabelWidth}
+              hintText={this.props.hintText}
+              grow="1"
+              hinter={this.props.hinter}
+              comboType={this.props.hinter}
+            />
+          ))}
+        />
+      </Container>
+    );
+  }
+
+  renderEditFileInput () {
+    return (
+      <Container kind="row-pane">
+        <input
+          type="file"
+          onChange={this.handleFileChange}
+          accept={this.props.accept}
+        />
+      </Container>
+    );
+  }
+  //#endregion
+
+  renderReadonly () {
     switch (this.props.kind) {
       case 'field':
-        return this.renderField ();
+        return this.renderReadonlyField ();
       case 'double-field':
-        return this.renderDoubleField ();
+        return this.renderReadonlyDoubleField ();
       case 'combo':
-        return this.renderCombo ();
+        return this.renderReadonlyCombo ();
       case 'radio':
-        return this.renderRadio ();
+        return this.renderReadonlyRadio ();
       case 'bool':
-        return this.renderBool ();
+        return this.renderReadonlyBool ();
       case 'hinter':
-        return this.renderHinter ();
+        return this.renderReadonlyHinter ();
       case 'file':
-        return this.renderFileInput ();
+        return this.renderReadonlyFileInput ();
       default:
-        return this.renderField ();
+        return this.renderReadonlyField ();
+    }
+  }
+
+  renderEdit () {
+    switch (this.props.kind) {
+      case 'field':
+        return this.renderEditField ();
+      case 'double-field':
+        return this.renderEditDoubleField ();
+      case 'combo':
+        return this.renderEditCombo ();
+      case 'radio':
+        return this.renderEditRadio ();
+      case 'bool':
+        return this.renderEditBool ();
+      case 'hinter':
+        return this.renderEditHinter ();
+      case 'file':
+        return this.renderEditFileInput ();
+      default:
+        return this.renderEditField ();
+    }
+  }
+
+  render () {
+    if (Bool.isTrue (this.props.readonly)) {
+      return this.renderReadonly ();
+    } else {
+      return this.renderEdit ();
     }
   }
 }
