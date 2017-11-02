c9af49af5d955579e6f4a0a21e32f4ce8d5184b4
Daniel Roux <roux@epsitec.ch>
2017-11-02 12:26:55 +0100

    Refactoring.

--- widgets/drag-cab/styles.js
+++ widgets/drag-cab/styles.js
@@ -5,35 +5,55 @@ import * as Bool from 'gadgets/boolean-helpers';
 /******************************************************************************/
 
 export default function styles (theme, props) {
-  const overlapX = '0px'; // overlap for hover
-  const overlapY = '1px'; // overlap for hover
+  const hoverOverlapX = '0px'; // overlap for hover
+  const hoverOverlapY = '1px'; // overlap for hover
+  const hoverBottomSpace = '2px'; // margin between two rows
 
-  let width = '100%';
-  let height = '100%';
+  const handleSpace = '4px'; // top/bottom space
+  const handleShift = '4px'; // shift from left
+  const handleThickness = '2px';
+
+  let detectWidth = '100%';
+  let deltectHeight = '100%';
   let cursor = 'move';
+  let handleLeft = '0px';
+  let handleTop = '0px';
+  let handleWidth = '100%';
+  let handleHeight = '100%';
+
   if (props.direction === 'horizontal') {
-    height = props.dragHandleHeight || theme.shapes.containerMargin;
+    deltectHeight = props.dragHandleHeight || theme.shapes.containerMargin;
     cursor = 'ew-resize';
+    handleLeft = handleSpace;
+    handleTop = handleShift;
+    handleWidth = `calc(100% - ${Unit.multiply (handleSpace, 2)} - ${hoverBottomSpace})`;
+    handleHeight = handleThickness;
   }
   if (props.direction === 'vertical') {
-    width = props.dragHandleWidth || theme.shapes.containerMargin;
+    detectWidth = props.dragHandleWidth || theme.shapes.containerMargin;
     cursor = 'ns-resize';
+    handleLeft = handleShift;
+    handleTop = handleSpace;
+    handleWidth = handleThickness;
+    handleHeight = `calc(100% - ${Unit.multiply (handleSpace, 2)} - ${hoverBottomSpace})`;
   }
 
   const detectStyle = {
     position: 'absolute',
     left: '0px',
     top: '0px',
-    width: width,
-    height: height,
+    width: detectWidth,
+    height: deltectHeight,
     cursor: cursor,
   };
   detectStyle[':hover'] = {
     top: '0px',
     left: '0px',
-    width: `calc(100% + ${Unit.multiply (overlapX, 2)})`,
-    height: `calc(100% + ${Unit.multiply (overlapY, 2)} - 2px)`,
-    margin: Unit.multiply (overlapY, -1) + ' ' + Unit.multiply (overlapX, -1),
+    width: `calc(100% + ${Unit.multiply (hoverOverlapX, 2)})`,
+    height: `calc(100% + ${Unit.multiply (hoverOverlapY, 2)} - ${hoverBottomSpace})`,
+    margin: Unit.multiply (hoverOverlapY, -1) +
+      ' ' +
+      Unit.multiply (hoverOverlapX, -1),
     borderLeft: '6px solid ' + theme.palette.dragAndDropHover,
     boxSizing: 'border-box',
     backgroundColor: 'rgba(0,0,0, 0.05)',
@@ -41,11 +61,11 @@ export default function styles (theme, props) {
 
   const handleStyle = {
     position: 'absolute',
-    left: '4px',
-    top: '4px',
-    width: '2px',
-    height: 'calc(100% - 8px - 2px)',
-    borderLeft: '2px dotted #bbb',
+    left: handleLeft,
+    top: handleTop,
+    width: handleWidth,
+    height: handleHeight,
+    borderLeft: `${handleThickness} dotted #bbb`,
     boxSizing: 'border-box',
   };
 
