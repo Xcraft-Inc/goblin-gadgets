a7599a82b9d191b926828a74cdb82c98a5942b13
Samuel Loup <loup@epsitec.ch>
2017-11-02 13:52:29 +0100

    Fix field hinter react error, but now, field  size is incorrect

--- widgets/field/widget.js
+++ widgets/field/widget.js
@@ -722,20 +722,18 @@ class Field extends Form {
         height={this.props.height}
         spacing={this.props.verticalSpacing}
       >
-        <Form
-          {...this.formConfigWithComponent (() => (
-            <Hinter
-              id={this.context.id}
-              labelText={this.props.labelText}
-              labelGlyph={this.props.labelGlyph}
-              labelWidth={this.props.labelWidth || defaultLabelWidth}
-              hintText={this.props.hintText}
-              grow="1"
-              hinter={this.props.hinter}
-              comboType={this.props.hinter}
-            />
-          ))}
-        />
+        <Form {...this.formConfig}>
+          <Hinter
+            id={this.context.id}
+            labelText={this.props.labelText}
+            labelGlyph={this.props.labelGlyph}
+            labelWidth={this.props.labelWidth || defaultLabelWidth}
+            hintText={this.props.hintText}
+            grow="1"
+            hinter={this.props.hinter}
+            comboType={this.props.hinter}
+          />
+        </Form>
       </Container>
     );
   }
