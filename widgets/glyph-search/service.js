2af22c10da86896bb276fb6101e4324460c61450
Samuel Loup <loup@epsitec.ch>
2017-11-14 10:08:07 +0100

    Fix tab text when opened manually

--- widgets/glyph-search/service.js
+++ widgets/glyph-search/service.js
@@ -6,20 +6,22 @@ const config = {
   kind: 'search',
   hinters: {
     glyph: {
-      onValidate: function (quest, selection) {
+      onValidate: function* (quest, selection) {
         const i = quest.openInventory ();
         const desk = i.use (quest.goblin.getX ('desktopId'));
+        const glyph = yield quest.warehouse.get ({path: selection.value});
         desk.addWorkitem ({
           workitem: {
             id: quest.uuidV4 (),
             name: 'glyph-workitem',
-            description: selection.text,
+            description: glyph.meta.info,
             view: 'default',
             icon: 'edit-pen',
             isInWorkspace: true,
             isClosable: true,
             payload: {
               entityId: selection.value,
+              entity: glyph,
             },
           },
           navigate: true,
