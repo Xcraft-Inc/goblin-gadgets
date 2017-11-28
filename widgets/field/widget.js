bf195f41ca2b212b4b349df9b56a13a9ccad8e75
Samuel Loup <loup@epsitec.ch>
2017-11-28 14:34:23 +0100

    Hinter selected field don't clean anymore on click

--- widgets/field/widget.js
+++ widgets/field/widget.js
@@ -1111,7 +1111,6 @@ class Field extends Form {
               width={this.props.labelWidth || defaultLabelWidth}
               hintText={this.props.hintText}
               grow="2"
-              onClick={() => this.setBackendValue (this.fullPath, null)}
             />
           : null}
         {props.existingValue
