0def984e592493a9c0d3de36ec2e02812d23f648
Daniel Roux <roux@epsitec.ch>
2018-02-01 15:02:43 +0100

    Optimize radio-list: with bool options no/yes or false/true, display a check-button.

--- widgets/radio-list/widget.js
+++ widgets/radio-list/widget.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Widget from 'laboratory/widget';
 import * as Bool from 'gadgets/boolean-helpers';
 
-import Container from 'gadgets/container/widget';
 import CheckButton from 'gadgets/check-button/widget';
 
 /******************************************************************************/
@@ -11,16 +10,51 @@ class RadioList extends Widget {
   constructor () {
     super (...arguments);
 
-    this.onButtonClicked = this.onButtonClicked.bind (this);
+    this.onRadioClicked = this.onRadioClicked.bind (this);
+    this.onCheckClicked = this.onCheckClicked.bind (this);
   }
 
-  onButtonClicked (index) {
+  onRadioClicked (index) {
     const x = this.props.selectionChanged;
     if (x) {
       x (index);
     }
   }
 
+  onCheckClicked () {
+    const x = this.props.selectionChanged;
+    if (x) {
+      const index = this.props.selectedIndex ^ 1; // swap (0 <-> 1)
+      x (index);
+    }
+  }
+
+  // Indicates whether the list of radio buttons can be reduced to a single check button.
+  get isBool () {
+    return (
+      this.props.list.length === 2 &&
+      ((this.props.list[0] === 'no' && this.props.list[1] === 'yes') ||
+        (this.props.list[0] === 'false' && this.props.list[1] === 'true'))
+    );
+  }
+
+  /******************************************************************************/
+
+  // Render a single check button, with the text of the second option (yes/true).
+  renderCheck () {
+    return (
+      <CheckButton
+        kind="check"
+        heightStrategy={this.props.heightStrategy}
+        text={this.props.list[1]}
+        focusable="true"
+        checked={Bool.toString (this.props.selectedIndex === 1)}
+        onClick={() => this.onCheckClicked ()}
+      />
+    );
+  }
+
+  // Render a single radio button.
   renderRow (text, index) {
     return (
       <CheckButton
@@ -30,11 +64,12 @@ class RadioList extends Widget {
         text={text}
         focusable="true"
         checked={Bool.toString (index === this.props.selectedIndex)}
-        onClick={() => this.onButtonClicked (index)}
+        onClick={() => this.onRadioClicked (index)}
       />
     );
   }
 
+  // Render a list of radio buttons.
   renderRows () {
     const result = [];
     let index = 0;
@@ -48,7 +83,7 @@ class RadioList extends Widget {
     const boxClass = this.styles.classNames.box;
     return (
       <div className={boxClass}>
-        {this.renderRows ()}
+        {this.isBool ? this.renderCheck () : this.renderRows ()}
       </div>
     );
   }
