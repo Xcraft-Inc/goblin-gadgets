6703c90efae338cfbef558f22d90f545257914d3
Jonny Quarta <quarta@epsitec.ch>
2018-10-16 12:35:21 +0200

    Adapt gadgets tooltip helpers to support translation

--- widgets/helpers/tooltip-helpers.js
+++ widgets/helpers/tooltip-helpers.js
@@ -1,5 +1,7 @@
-function prepare(text) {
-  return text ? text.replace(/<br\/>/i, '\n') : text;
+const {T} = require('nabu/helpers/tooltip-helpers');
+
+function prepare(text, state, widget) {
+  return text ? T(state, text, widget).replace(/<br\/>/i, '\n') : text;
 }
 
 //-----------------------------------------------------------------------------
