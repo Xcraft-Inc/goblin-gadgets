6a1671d5e680f053a3626140057bc9bfe850d0d2
Samuel Loup <samuel.loup@gmail.com>
2018-11-08 13:21:13 +0100

    Fix bug with textfield loosing data when unmounted

--- widgets/text-field/widget.js
+++ widgets/text-field/widget.js
@@ -352,6 +352,7 @@ class TextField extends Widget {
       />
     ) : (
       <Control
+        persist
         className={`${fieldClass} ${inputClass}`}
         component={Field}
         changeAction={beforeChange}
