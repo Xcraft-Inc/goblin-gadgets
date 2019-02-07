97bb09cf805ec5212e4a0f6ed70c654ba12c2af4
Jonny Quarta <quarta@epsitec.ch>
2019-02-07 17:41:56 +0100

    Remove import for div and a

--- widgets/button/widget.js
+++ widgets/button/widget.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {div, a} from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import Widget from 'laboratory/widget';
 import MouseTrap from 'mousetrap';
