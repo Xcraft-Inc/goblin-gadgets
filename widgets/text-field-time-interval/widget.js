6fe51a4eb9c0023bfd243f21ce7ad4a171768034
Samuel Loup <samuel.loup@gmail.com>
2019-02-27 09:42:54 +0100

    T

--- widgets/text-field-time-interval/widget.js
+++ widgets/text-field-time-interval/widget.js
@@ -1,14 +1,9 @@
 //T:2019-02-27
-<<<<<<< HEAD
 import T from 't';
-=======
->>>>>>> 086b89ad22349babe9eb2ff88498386ed2ef2bb7
 import React from 'react';
 import Widget from 'laboratory/widget';
-import T from 't';
 const Bool = require('gadgets/helpers/bool-helpers');
 import {time as TimeConverters} from 'xcraft-core-converters';
-
 import TextFieldTyped from 'gadgets/text-field-typed/widget';
 import Button from 'gadgets/button/widget';
 
@@ -38,11 +33,7 @@ class TextFieldTimeInterval extends Widget {
         labelText={this.props.labelText}
         labelGlyph={this.props.labelGlyph}
         labelWidth={this.props.labelWidth}
-<<<<<<< HEAD
         hintText={this.props.startHintText || T('Début', 'heure de début')}
-=======
-        hintText={this.props.startHintText || T('Début')}
->>>>>>> 086b89ad22349babe9eb2ff88498386ed2ef2bb7
         tooltip={this.props.startTooltip || T('Heure de début')}
         selectAllOnFocus={this.props.selectAllOnFocus}
         readonly={this.props.readonly}
@@ -72,11 +63,7 @@ class TextFieldTimeInterval extends Widget {
         }
         type="time"
         labelWidth="0px"
-<<<<<<< HEAD
         hintText={this.props.endHintText || T('Fin', 'heure de fin')}
-=======
-        hintText={this.props.endHintText || T('Fin')}
->>>>>>> 086b89ad22349babe9eb2ff88498386ed2ef2bb7
         tooltip={this.props.endTooltip || T('Heure de fin')}
         selectAllOnFocus={this.props.selectAllOnFocus}
         readonly={this.props.readonly}
