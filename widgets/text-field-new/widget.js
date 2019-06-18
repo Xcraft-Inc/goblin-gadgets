590f29bb3d12f29966f7c94fc6106c2fa6504eec
Yannick Vessaz <vessaz@epsitec.ch>
2019-06-18 11:51:11 +0200

    Handle prop model in new text-field for backward compatibility

--- widgets/text-field-new/widget.js
+++ widgets/text-field-new/widget.js
@@ -1,6 +1,19 @@
-import TextFieldNC from '../TextField-nc/widget';
+import React from 'react';
+import TextFieldNC from '../text-field-nc/widget';
 import withC from 'goblin-laboratory/widgets/connect-helpers/with-c';
+import C from 'goblin-laboratory/widgets/connect-helpers/c';
+import wrapRawInput from 'goblin-laboratory/widgets/input-wrapper/widget.js';
 
-const TextField = withC(TextFieldNC, {value: 'onChange'});
+const TextField = withC(wrapRawInput(TextFieldNC), {value: 'onChange'});
 TextField.displayName = 'TextFieldNew';
-export default TextField;
+
+// export default TextField;
+
+// Convert model to value for backward compatibility, should be removed
+export default props => {
+  let {value, model, ...otherProps} = props;
+  if (model) {
+    value = C(model);
+  }
+  return <TextField {...otherProps} value={value} />;
+};
