c866c82866c01fa2c47c7f2fad56e49fe1bbe5e7
Daniel Roux <roux@epsitec.ch>
2019-06-26 10:37:43 +0200

    Add scenarios.

---
+++ widgets/label/scenarios.js
@@ -0,0 +1,17 @@
+export default {
+  default: {
+    text: 'Adresse du client',
+    width: '200px',
+  },
+  markdown: {
+    kind: 'markdown',
+    text: '```# Bonjour\n\n- Madame\n- Monsieur\n```',
+    width: '300px',
+  },
+  big: {
+    kind: 'big-center',
+    text: 'Grand titre',
+    width: '200px',
+  },
+  empty: {},
+};
