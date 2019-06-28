c46ca9d0dcbcd889905f40c4f926b73419e6b942
Daniel Roux <roux@epsitec.ch>
2019-06-28 18:14:09 +0200

    WIP: Second step for testing TextFieldComboNC in WidgetDoc.

--- widgets/text-field-combo-nc/scenarios.js
+++ widgets/text-field-combo-nc/scenarios.js
@@ -1,10 +1,65 @@
 export default [
   {
-    name: 'month',
+    name: 'team',
     props: {
+      list: 'team',
+      hintText: 'Collaborateur',
+      width: '200px',
+      menuType: 'wrap',
+      readonly: true,
+    },
+  },
+  {
+    name: 'colors',
+    props: {
+      list: 'colors',
+      width: '100px',
+      menuType: 'wrap',
+      readonly: true,
+    },
+  },
+  {
+    name: 'months',
+    props: {
+      list: 'months',
       hintText: "Mois de l'échéance",
       width: '200px',
       menuType: 'wrap',
+      readonly: true,
+    },
+  },
+  {
+    name: 'transports',
+    props: {
+      list: 'transports',
+      hintText: 'Moyen de transport',
+      width: '200px',
+      menuType: 'wrap',
+      readonly: true,
+    },
+  },
+  {
+    name: 'localities',
+    props: {
+      list: 'localities',
+      hintText: 'Domicile',
+      readonly: true,
+    },
+  },
+  {
+    name: 'strange',
+    props: {
+      list: 'strange',
+      shape: 'rounded',
+      readonly: true,
+    },
+  },
+  {
+    name: 'multiline',
+    props: {
+      list: 'multiline',
+      width: '300px',
+      rows: '3',
       readonly: true,
     },
   },
