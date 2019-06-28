4f25a575850e6235341e8ba13f8c289d5e3b0d7c
Daniel Roux <roux@epsitec.ch>
2019-06-28 18:29:44 +0200

    Add scenarios for Table.

--- widgets/table/scenarios.js
+++ widgets/table/scenarios.js
@@ -1,5 +1,46 @@
 export default [
   {
+    name: 'little',
+    props: {
+      data: 'T1',
+    },
+  },
+  {
+    name: 'big',
+    props: {
+      data: 'T3',
+      frame: true,
+    },
+  },
+  {
+    name: 'filter',
+    props: {
+      data: 'T4',
+      sortingColumns: 'S1',
+      frame: true,
+      hasButtons: true,
+    },
+  },
+  {
+    name: 'levels',
+    props: {
+      data: 'T5',
+      frame: true,
+      headerWithoutHorizontalSeparator: true,
+      fontSizeStrategy: 'decrease',
+    },
+  },
+  {
+    name: 'scroll',
+    props: {
+      data: 'T5',
+      frame: true,
+      hasButtons: true,
+      headerWithoutHorizontalSeparator: true,
+      height: '200px',
+    },
+  },
+  {
     name: 'empty',
     props: {},
   },
