3dfddf942a0a8819a163f6be111f38257fa8c2f0
Daniel Roux <roux@epsitec.ch>
2019-07-02 16:49:04 +0200

    Fix bug.

--- widgets/widget-doc-preview-container/styles.js
+++ widgets/widget-doc-preview-container/styles.js
@@ -35,7 +35,6 @@ export default function styles(theme, props) {
       : null;
 
   const previewContainer = {
-    position: 'relative',
     margin: '10px 0px',
     flexGrow: '1',
     display: 'flex',
