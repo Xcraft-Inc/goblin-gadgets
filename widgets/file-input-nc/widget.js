9e2aca80eecb49f504cace14ab21c313334b11f7
Gillian Fries <fries@epsitec.ch>
2019-08-23 17:30:36 +0200

    Improve FileInput and add style with theme

--- widgets/file-input-nc/widget.js
+++ widgets/file-input-nc/widget.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Widget from 'goblin-laboratory/widgets/widget';
+import T from 't';
+import Label from '../label/widget';
 
+let count = 0;
 export default class FileInputNC extends Widget {
   constructor() {
     super(...arguments);
     this.handleFileChange = this.handleFileChange.bind(this);
+    this.state = {files: []};
+    this.id = 'FileInput-' + count;
+    count++;
   }
 
   handleFileChange(ev) {
@@ -14,23 +20,64 @@ export default class FileInputNC extends Widget {
     for (let i = 0; i < fileList.length; i++) {
       files.push(fileList[i].path);
     }
+    this.setState({files});
     if (files.length === 1) {
       if (this.props.onChange) {
         this.props.onChange(files[0]);
       }
-    } else {
-      throw new Error('Not implemented');
+    } else if (files.length > 1) {
+      if (this.props.onChange) {
+        this.props.onChange(files);
+      }
     }
   }
 
   render() {
+    const filesCount = this.state.files.length;
+    let label = T('Choix du fichier');
+    if (this.props.multiple) {
+      label = T('Choix du/des fichiers');
+    }
+    if (filesCount === 1) {
+      label = this.state.files[0];
+      label = label.substring(label.lastIndexOf('\\') + 1);
+    } else if (filesCount > 1) {
+      label = T('{filesCount} fichiers selectionnés', null, {filesCount});
+    }
+
+    if (this.props.multiple) {
+      return (
+        <React.Fragment>
+          <input
+            id={this.id}
+            type="file"
+            onChange={this.handleFileChange}
+            accept={this.props.accept}
+            ref={this.props.inputRef}
+            className={this.styles.classNames.inputfile}
+            multiple
+          />
+          <label for={this.id} className={this.styles.classNames.label}>
+            <Label text={label} glyph="solid/upload" />
+          </label>
+        </React.Fragment>
+      );
+    }
+
     return (
-      <input
-        type="file"
-        onChange={this.handleFileChange}
-        accept={this.props.accept}
-        ref={this.props.inputRef}
-      />
+      <React.Fragment>
+        <input
+          id={this.id}
+          type="file"
+          onChange={this.handleFileChange}
+          accept={this.props.accept}
+          ref={this.props.inputRef}
+          className={this.styles.classNames.inputfile}
+        />
+        <label for={this.id} className={this.styles.classNames.label}>
+          <Label text={label} glyph="solid/upload" />
+        </label>
+      </React.Fragment>
     );
   }
 }
