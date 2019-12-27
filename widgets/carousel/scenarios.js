743184aa108145406d2ccaac0f59d1fa58532bad
Daniel Roux <roux@epsitec.ch>
2019-12-27 18:13:41 +0100

    Gadgets: Carousel: Improve WidgetDoc.

--- widgets/carousel/scenarios.js
+++ widgets/carousel/scenarios.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CarouselItem from '../carousel-item/widget';
+import Button from '../button/widget';
 
 /******************************************************************************/
 
@@ -11,31 +12,41 @@ function getChildren(width, height, itemMargin) {
         height={height}
         itemMargin={itemMargin}
         backgroundColor="yellow"
-      />
+      >
+        <Button text="1/5" />
+      </CarouselItem>
       <CarouselItem
         width={width}
         height={height}
         itemMargin={itemMargin}
         backgroundColor="orange"
-      />
+      >
+        <Button text="2/5" />
+      </CarouselItem>
       <CarouselItem
         width={width}
         height={height}
         itemMargin={itemMargin}
         backgroundColor="red"
-      />
+      >
+        <Button text="3/5" />
+      </CarouselItem>
       <CarouselItem
         width={width}
         height={height}
         itemMargin={itemMargin}
         backgroundColor="purple"
-      />
+      >
+        <Button text="4/5" />
+      </CarouselItem>
       <CarouselItem
         width={width}
         height={height}
         itemMargin={itemMargin}
         backgroundColor="blue"
-      />
+      >
+        <Button text="5/5" />
+      </CarouselItem>
     </React.Fragment>
   );
 }
@@ -44,7 +55,7 @@ function getChildren(width, height, itemMargin) {
 
 export default [
   {
-    name: 'Default',
+    name: 'Spaced',
     props: {
       maxWidth: '660px',
       itemWidth: '220px',
@@ -61,7 +72,7 @@ export default [
     },
   },
   {
-    name: 'Compact',
+    name: 'Near',
     props: {
       maxWidth: '606px',
       itemWidth: '202px',
@@ -75,6 +86,23 @@ export default [
       responsive: true,
       touch: true,
       children: getChildren('200px', '300px', '1px'),
+    },
+  },
+  {
+    name: 'Compact',
+    props: {
+      maxWidth: '600px',
+      itemWidth: '200px',
+      itemMargin: '0px',
+      buttonsCenter: '0px',
+      buttonsShape: 'circle',
+      buttonsSize: '40px',
+      buttonsShift: '5px',
+      cycling: 'blocked',
+      navigator: 'bullets',
+      responsive: true,
+      touch: true,
+      children: getChildren('200px', '300px', '0px'),
     },
   },
   {
