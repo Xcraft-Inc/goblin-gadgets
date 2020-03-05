25e69ca9a5702fad0ac2d5062daad0cf744a03e6
Daniel Roux <roux@epsitec.ch>
2020-03-05 13:32:15 +0100

    Westeros: Refactoring svg for component Ticket.

--- widgets/ticket/getOutlinePath.js
+++ widgets/ticket/getOutlinePath.js
@@ -11,44 +11,40 @@ function toInt(value) {
 
 // Move to absolute position.
 function moveTo(path, x, y) {
-  path += 'M ' + x + ' ' + y + ' ';
-  return path;
+  path.push('M ' + x + ' ' + y);
 }
 
 // Line to relative position.
 function lineTo(path, dx, dy) {
-  path += 'l ' + dx + ' ' + dy + ' ';
-  return path;
+  path.push('l ' + dx + ' ' + dy);
 }
 
 // Arc to relative position.
 function arcTo(path, r, cx, cy, sweepFlag) {
   // rx ry x-axis-rotation large-arc-flag sweep-flag x y
   // see http://www.w3.org/TR/SVG/paths.html#PathDataEllipticalArcCommands
-  path += 'a ' + r + ' ' + r + ' 0 0 ' + sweepFlag + ' ' + cx + ' ' + cy + ' ';
-  // path += 'a ' + r + ' '  + r + ' 0 0 0 ' + cx + ' ' + cy + ' ';
-  return path;
+  path.push('a ' + r + ' ' + r + ' 0 0 ' + sweepFlag + ' ' + cx + ' ' + cy);
 }
 
 // Close path.
 function close(path) {
-  path += 'z';
-  return path;
+  path.push('z');
 }
 
 // Draw _n_n_n_n_n_n_n_n_n_n_n_n_n_
 function horizontalDash(path, r, len, dx) {
   const step = parseInt(dx / len);
   const over = (dx - len * step) / 2;
-  path = lineTo(path, over + r, 0);
+  lineTo(path, over + r, 0);
   let i = 0;
   for (i = 0; i < step - 1; i++) {
-    path = lineTo(path, len - r - r, 0);
-    path = arcTo(path, r, r + r, 0, 0);
+    lineTo(path, len - r - r, 0);
+    arcTo(path, r, r + r, 0, 0);
   }
-  path = lineTo(path, len - r + over, 0);
-  return path;
+  lineTo(path, len - r + over, 0);
 }
+
+/******************************************************************************/
 
 export default function getOutlinePath(theme, shape, width, height) {
   const r = toInt(theme.shapes.ticketCornerRadius);
@@ -59,43 +55,43 @@ export default function getOutlinePath(theme, shape, width, height) {
   const hasTopSerration = TicketHelpers.hasTopSerration(shape);
   const hasBottomSerration = TicketHelpers.hasBottomSerration(shape);
 
-  let path = '';
+  const path = [];
   if (!hasTopSerration && hasBottomSerration) {
     // Serration only on bottom.
-    path = moveTo(path, 0, 0);
-    path = lineTo(path, w, 0);
-    path = lineTo(path, 0, h - r);
-    path = arcTo(path, r, -r, r, 0); // bottom-right corner
-    path = horizontalDash(path, -s, -s * 3.5, -(w - r - r));
-    path = arcTo(path, r, -r, -r, 0); // bottom-left corner
-    path = close(path);
+    moveTo(path, 0, 0);
+    lineTo(path, w, 0);
+    lineTo(path, 0, h - r);
+    arcTo(path, r, -r, r, 0); // bottom-right corner
+    horizontalDash(path, -s, -s * 3.5, -(w - r - r));
+    arcTo(path, r, -r, -r, 0); // bottom-left corner
+    close(path);
   } else if (hasTopSerration && !hasBottomSerration) {
     // Serration only on top.
-    path = moveTo(path, 0, r);
-    path = arcTo(path, r, r, -r, 0); // top-left corner
-    path = horizontalDash(path, s, s * 3.5, w - r - r);
-    path = arcTo(path, r, r, r, 0); // top-right corner
-    path = lineTo(path, 0, h - r);
-    path = lineTo(path, -w, 0);
-    path = close(path);
+    moveTo(path, 0, r);
+    arcTo(path, r, r, -r, 0); // top-left corner
+    horizontalDash(path, s, s * 3.5, w - r - r);
+    arcTo(path, r, r, r, 0); // top-right corner
+    lineTo(path, 0, h - r);
+    lineTo(path, -w, 0);
+    close(path);
   } else if (!hasTopSerration && !hasBottomSerration) {
     // No Serration.
-    path = moveTo(path, 0, 0);
-    path = lineTo(path, w, 0);
-    path = lineTo(path, 0, h);
-    path = lineTo(path, -w, 0);
-    path = close(path);
+    moveTo(path, 0, 0);
+    lineTo(path, w, 0);
+    lineTo(path, 0, h);
+    lineTo(path, -w, 0);
+    close(path);
   } else {
     // Serration on top and bottom.
-    path = moveTo(path, 0, r);
-    path = arcTo(path, r, r, -r, 0); // top-left corner
-    path = horizontalDash(path, s, s * 3.5, w - r - r);
-    path = arcTo(path, r, r, r, 0); // top-right corner
-    path = lineTo(path, 0, h - r - r);
-    path = arcTo(path, r, -r, r, 0); // bottom-right corner
-    path = horizontalDash(path, -s, -s * 3.5, -(w - r - r));
-    path = arcTo(path, r, -r, -r, 0); // bottom-left corner
-    path = close(path);
+    moveTo(path, 0, r);
+    arcTo(path, r, r, -r, 0); // top-left corner
+    horizontalDash(path, s, s * 3.5, w - r - r);
+    arcTo(path, r, r, r, 0); // top-right corner
+    lineTo(path, 0, h - r - r);
+    arcTo(path, r, -r, r, 0); // bottom-right corner
+    horizontalDash(path, -s, -s * 3.5, -(w - r - r));
+    arcTo(path, r, -r, -r, 0); // bottom-left corner
+    close(path);
   }
-  return path;
+  return path.join(' ');
 }
