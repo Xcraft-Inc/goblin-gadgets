a72a86e687735978a96759b72214a777e97c5d33
Daniel Roux <roux@epsitec.ch>
2020-06-05 07:43:37 +0200

    Westeros: Suppress old component.

--- widgets/text-field-combo/widget.js
+++ widgets/text-field-combo/widget.js
@@ -1,2 +1,21 @@
-// export {default} from '../text-field-combo-old/widget';
-export {default} from '../text-field-combo-new/widget';
+import React from 'react';
+import withC from 'goblin-laboratory/widgets/connect-helpers/with-c';
+import C from 'goblin-laboratory/widgets/connect-helpers/c';
+import TextFieldComboNC from '../text-field-combo-nc/widget';
+
+const TextFieldCombo = withC(TextFieldComboNC, {selectedId: 'onChange'});
+TextFieldCombo.displayName = 'TextFieldCombo';
+
+// export default TextFieldCombo;
+
+// Convert model to value for backward compatibility, should be removed
+export default (props) => {
+  let {selectedId, model, list, listModel, ...otherProps} = props;
+  if (model) {
+    selectedId = C(model);
+  }
+  if (listModel) {
+    list = C(listModel);
+  }
+  return <TextFieldCombo {...otherProps} selectedId={selectedId} list={list} />;
+};
