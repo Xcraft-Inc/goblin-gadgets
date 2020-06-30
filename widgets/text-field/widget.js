b685a8f0678fd2bf30a0c8859522849eb482485e
Daniel Roux <roux@epsitec.ch>
2020-06-30 10:24:38 +0200

    Westeros: Delete TextFieldNew (move code to TextField).

--- widgets/text-field/widget.js
+++ widgets/text-field/widget.js
@@ -1,2 +1,18 @@
-// export {default} from '../text-field-old/widget';
-export {default} from '../text-field-new/widget';
+import React from 'react';
+import withC from 'goblin-laboratory/widgets/connect-helpers/with-c';
+import C from 'goblin-laboratory/widgets/connect-helpers/c';
+import TextFieldNC from '../text-field-nc/widget';
+
+const TextField = withC(TextFieldNC, {value: 'onChange'});
+TextField.displayName = 'TextField';
+
+// export default TextField;
+
+// Convert model to value for backward compatibility, should be removed
+export default (props) => {
+  let {value, model, ...otherProps} = props;
+  if (model) {
+    value = C(model);
+  }
+  return <TextField {...otherProps} value={value} />;
+};
