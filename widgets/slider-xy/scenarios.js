003c003be30fe4c1c475e2dee28c8150a9b9ca6f
Daniel Roux <roux@epsitec.ch>
2020-09-01 10:03:28 +0200

    Westeros: Fix scenarios for WidgetDoc.

--- widgets/slider-xy/scenarios.js
+++ widgets/slider-xy/scenarios.js
@@ -1,44 +1,15 @@
 export default [
   {
-    name: 'horizontal',
+    name: 'default',
     props: {
-      direction: 'horizontal',
-      barColor: '#f00',
-      width: '150px',
-      value: 25,
-    },
-  },
-  {
-    name: 'vertical',
-    props: {
-      direction: 'vertical',
-      barColor: '#f00',
-      height: '150px',
-      value: 25,
-    },
-  },
-  {
-    name: 'gradient',
-    props: {
-      direction: 'horizontal',
-      width: '150px',
-      value: 25,
-      gradient: '1to2',
-      gradientColor1: '#ff0',
-      gradientColor2: '#f00',
-      gliderSize: 'large',
-    },
-  },
-  {
-    name: 'rainbow',
-    props: {
-      direction: 'horizontal',
-      width: '150px',
-      value: 25,
-      gradient: 'rainbow',
-      gliderSize: 'large',
-      cabSize: 'large',
-      cabType: 'thin',
+      width: '200px',
+      height: '200px',
+      cabSize: 'default',
+      marginSize: 'small',
+      marginStyle: 'none',
+      draggingScale: 1,
+      hue: 50,
+      value: '75;95',
     },
   },
   {
