e5e8fb01e97b3b7296c9d5a2e091e80261b476db
Samuel Loup <loup@epsitec.ch>
2021-09-21 15:31:13 +0200

    impl. <CommandButton

---
+++ widgets/command-button/widget.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Widget from 'goblin-laboratory/widgets/widget';
+import Button from 'goblin-gadgets/widgets/button/widget';
+
+class CommandButton extends Widget {
+  constructor() {
+    super(...arguments);
+  }
+
+  render() {
+    const {command, showDisabled, ...others} = this.props;
+    if (
+      process &&
+      process.env &&
+      process.env.NODE_ENV === 'development' &&
+      !this.registry[command]
+    ) {
+      //show the bad command in UI as a disabled button
+      return <Button {...others} disabled="true" text={command} />;
+    }
+
+    if (!this.canDo(command)) {
+      if (showDisabled) {
+        return <Button {...others} disabled="true" />;
+      } else {
+        return null;
+      }
+    }
+    return <Button {...others} />;
+  }
+}
+
+export default Widget.connect((state) => {
+  const clientSessionId = state.get(`backend.${window.labId}.clientSessionId`);
+  const rank = state.get(`backend.login-session@${clientSessionId}.rank`);
+  return {
+    rank,
+  };
+})(CommandButton);
