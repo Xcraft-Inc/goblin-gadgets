dbf13293c1b9f56a350648efb675497a22e7494c
Mathieu Schroeter <mathieu@schroetersa.ch>
2023-05-04 23:58:51 +0200

    Cosmetics

--- test/code-parser.spec.js
+++ test/code-parser.spec.js
@@ -5,57 +5,59 @@ const parseCode = require('../widgets/widget-doc-preview/parse-code.js');
 
 //-----------------------------------------------------------------------------
 
-describe('Test parseCode function', function () {
-  it('#Test parseCode string', function () {
-    const code = `<Button onClick="alert" textColor="task" 
+describe('goblin.gadgets', function () {
+  describe('parseCode function', function () {
+    it('parseCode string', function () {
+      const code = `<Button onClick="alert" textColor="task"
     shortcut="_ctrl_+A" visibility={true} show="true"/>`;
-    const res = parseCode(code);
-    assert.deepStrictEqual(res, {
-      onClick: 'alert',
-      textColor: 'task',
-      shortcut: '_ctrl_+A',
-      visibility: true,
-      show: 'true',
+      const res = parseCode(code);
+      assert.deepStrictEqual(res, {
+        onClick: 'alert',
+        textColor: 'task',
+        shortcut: '_ctrl_+A',
+        visibility: true,
+        show: 'true',
+      });
     });
-  });
 
-  it('#Test parseCode boolean string', function () {
-    const code = `<Button visibility={"true"}/>`;
-    const res = parseCode(code);
-    assert.deepStrictEqual(res, {
-      visibility: 'true',
+    it('parseCode boolean string', function () {
+      const code = `<Button visibility={"true"}/>`;
+      const res = parseCode(code);
+      assert.deepStrictEqual(res, {
+        visibility: 'true',
+      });
     });
-  });
 
-  it('#Test parseCode boolean', function () {
-    const code = `<Button visibility={true} show={true}/>`;
-    const res = parseCode(code);
-    assert.deepStrictEqual(res, {
-      visibility: true,
-      show: true,
+    it('parseCode boolean', function () {
+      const code = `<Button visibility={true} show={true}/>`;
+      const res = parseCode(code);
+      assert.deepStrictEqual(res, {
+        visibility: true,
+        show: true,
+      });
     });
-  });
 
-  it('#Test parseCode number', function () {
-    const code = `<Button width={100} height={100}/>`;
-    const res = parseCode(code);
-    assert.deepStrictEqual(res, {
-      width: 100,
-      height: 100,
+    it('parseCode number', function () {
+      const code = `<Button width={100} height={100}/>`;
+      const res = parseCode(code);
+      assert.deepStrictEqual(res, {
+        width: 100,
+        height: 100,
+      });
     });
-  });
 
-  it('#Test parseCode string, boolean, number and EOF', function () {
-    const code = `<Button onClick="alert" textColor="task" 
+    it('parseCode string, boolean, number and EOF', function () {
+      const code = `<Button onClick="alert" textColor="task"
     shortcut="_ctrl_+A" visibility={true} size={100} show="true"/>`;
-    const res = parseCode(code);
-    assert.deepStrictEqual(res, {
-      onClick: 'alert',
-      textColor: 'task',
-      shortcut: '_ctrl_+A',
-      visibility: true,
-      size: 100,
-      show: 'true',
+      const res = parseCode(code);
+      assert.deepStrictEqual(res, {
+        onClick: 'alert',
+        textColor: 'task',
+        shortcut: '_ctrl_+A',
+        visibility: true,
+        size: 100,
+        show: 'true',
+      });
     });
   });
 });
